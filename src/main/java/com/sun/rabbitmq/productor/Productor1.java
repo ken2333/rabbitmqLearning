b7313934f50dcaaa69585a382ffe51cc3b33db4f
ken2333 <1176497634@qq.com>
2019-07-02 00:10:26 +0800

    初始化

---
+++ src/main/java/com/sun/rabbitmq/productor/Productor1.java
@@ -0,0 +1,15 @@
+package com.sun.rabbitmq.productor;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author ken
+ * @date 2019/7/2  0:09
+ * @description
+ */
+
+@Component
+public class Productor1 {
+
+
+}
